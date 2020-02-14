23d7c843ce2d4325b65bd1b94319d1660962db96
Manuel CABARCOS BAULINA <crac.master@hotmail.com>
2020-02-14 10:46:19 +0100

    chore : prototyping

--- src/main/java/Orchestrator/Orchestrator.java
+++ src/main/java/Orchestrator/Orchestrator.java
@@ -1,12 +1,21 @@
 package Orchestrator;
+import models.ComponentDTO;
+
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Orchestrator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println(4);
         Jsonb jsonb = JsonbBuilder.create();
-
+        FileReader reader = new FileReader("/home/manu/Documentos/m2/scaa/src/main/resources/component.json");
+        List<ComponentDTO> l =jsonb.fromJson(reader,new ArrayList<ComponentDTO>(){}.getClass().getGenericSuperclass());
+        System.out.println(l.get(0).getName());
     }
 }
+
--- src/main/java/importExport/Converter.java
+++ src/main/java/importExport/Converter.java
@@ -1,20 +1,25 @@
 package importExport;
 
 import models.Component;
+import models.ComponentDTO;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 
 public class Converter {
 
-    public List<Component> importFromJson(Reader r){
+    public List<Component> importFromJson(Reader reader){
         try{
-
+            Jsonb jsonb = JsonbBuilder.create();
+            List<ComponentDTO> l =jsonb.fromJson(reader,new ArrayList<ComponentDTO>(){}.getClass().getGenericSuperclass());
+            return null;
         }catch (Exception e){
             return Collections.EMPTY_LIST;
         }
-        return Collections.EMPTY_LIST;
     }
 }
